29d8dd93a1b289710a0f3b4c76b6d6e7dcf13f37
erenşenel <93876407+erensenell@users.noreply.github.com>
2022-08-21 20:11:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function generateQR() {
     document.querySelector("#qr-image").style.display = "block";
     let QRtext = document.querySelector("#text").value;
     if(QRtext.trim().length == 0) {
-        document.querySelector("#qr-image .error").innerHTML = "Please enter text";
+        document.querySelector("#qr-image .error").innerHTML = "Lütfen bir yazı giriniz!";
         document.querySelector("#img").style.display = "none";
     } else {
         document.querySelector("#img").style.display = "block";
